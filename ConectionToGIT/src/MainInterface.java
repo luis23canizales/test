da862390a95de0eff809d0681c114b593d8c4298
1451374 <1451374@22HW020523.SOAM.TCS.com>
2018-02-06 11:34:20 -0600

    Aqui se podra consultar la primera prueba de COMMIT.

---
+++ ConectionToGIT/src/MainInterface.java
@@ -0,0 +1,20 @@
+
+public class MainInterface {
+
+	public static void main(String[] args) {
+
+		Valores test = new Valores();
+		
+		saludosMxicanos();
+		test.Mensajito();
+	}
+		private static void saludosMxicanos(){
+			
+			System.out.println("FUNCIONO EL COMMIT 1!!!");
+			System.out.println("Compatriotas Mexicanos");
+			System.out.println("Les deseamos un feliz 5 de Febrero");
+		}
+		
+	}
+
+
---
+++ ConectionToGIT/src/Valores.java
@@ -0,0 +1,8 @@
+
+public class Valores {
+	
+	int a = 1000;
+
+	public static void Mensajito() {
+	System.out.println("Mensaje de prueba para: ");	}
+}
